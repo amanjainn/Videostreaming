8ec0a19e9941e16d1d358840f2cbc9b07427c2a1
Aman <amangrand@gmail.com>
2020-06-27 11:19:12 +0530

    Rtmp Server added

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,12 +1,44 @@
 import React from 'react'
+import flv from 'flv.js'
 import { connect } from 'react-redux'
 import {fetchStream} from '../../actions';
 
 
 class StreamShow extends React.Component{
-    componentDidMount(){
-        this.props.fetchStream(this.props.match.params.id);
+
+    constructor(props){
+        super(props)
+        this.videoRef = React.createRef()
     }
+    componentDidMount(){
+        this.props.fetchStream(this.props.match.params.id);  
+        this.buildplayer();
+
+    }
+
+
+    componentDidUpdate(){
+        this.buildplayer();
+    }
+
+  componentWillUnmount(){
+      this.player.destroy();
+  }
+
+    buildplayer=()=>{
+        if(this.player || !this.props.stream){
+            return;
+        }
+        const {id}=this.props.match.params;
+        this.player=flv.createPlayer({
+            type:'flv',
+            url:`https//localhost:8000/live/${id}.flv`
+        })
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
+    }
+
+
 
   render(){
       if(!this.props.stream){
@@ -16,6 +48,7 @@ class StreamShow extends React.Component{
 
   return(
        <div>
+           <video ref={this.videoRef}  style={{width: '50%'}} controls/>
            <h1>{ this.props.stream.title }</h1>
            <h5>{this.props.stream.description}</h5>
       </div>
